40dd454278c0797c82ab2e02aaab9d16d68c58a1
Sam Maton <sam.maton91@gmail.com>
2025-02-05 12:15:52 +0000

    setup base template, nav and embeded file system

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.New("home").ParseFiles("ui/html/base.html", "ui/html/pages/home.html")
+	t, err := template.New("home").ParseFiles("ui/html/base.html", "ui/html/partials/nav.html", "ui/html/pages/home.html")
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 	t.ExecuteTemplate(w, "base", nil)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,9 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/sam-maton/go-ecomm/ui"
+)
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.FS(ui.Files))
+	mux.Handle("GET /static/", fileServer)
 
 	mux.HandleFunc("/", app.home)
 	return mux
